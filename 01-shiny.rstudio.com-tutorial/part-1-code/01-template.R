41a42425d4d112061ad8a96e9299fb48b81f5c0c
Naftali Brigitta <naftaligunawan@gmail.com>
2022-12-01 11:56:22 +0700

    woi halo

--- 01-shiny.rstudio.com-tutorial/part-1-code/01-template.R
+++ 01-shiny.rstudio.com-tutorial/part-1-code/01-template.R
@@ -2,7 +2,7 @@ library(shiny)
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Babi"),
   
   # Sidebar with a slider input for number of bins
   sidebarLayout(
